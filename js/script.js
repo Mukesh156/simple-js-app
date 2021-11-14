021d63d9a775ba7e17dfadefa311441f23141cfc
Mukesh156 <93263100+Mukesh156@users.noreply.github.com>
2021-11-14 19:49:05 +0100

    Update script.js
    created pokemonList array

--- js/script.js
+++ js/script.js
@@ -1,3 +1,17 @@
-alert ('Hello World');
-let favoriteFood = 'Biryani';
-document.write(favoriteFood);
+let pokemonList = [
+    {
+      name: 'Bulbasaur',
+      height: 0.7,
+      type: ['grass', 'poison']
+    },
+    {
+      name: 'Beedrill',
+      height: 1,
+      type: ['bug']
+    },
+    {
+      name: 'Pikachu',
+      height: 0.4,
+      type: ['electric']
+    },
+  ];
